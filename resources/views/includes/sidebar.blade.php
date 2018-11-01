fe527c9769b6369bb625fef6bd3f0abfdb0b3678
Yose purnawan <purnawan.yose@gmail.com>
2018-11-02 00:20:28 +0700

    Initial Project

---
+++ resources/views/includes/sidebar.blade.php
@@ -0,0 +1,56 @@
+<div class="sidebar" style="background-color: #283e4a;" data-image="assets/img/sidebar-4.jpg">
+
+<!--
+
+    Tip 1: you can change the color of the sidebar using: data-color="blue | azure | green | orange | red | purple"
+    Tip 2: you can also add an image using data-image tag
+
+-->
+
+    <div class="sidebar-wrapper">
+        <div class="logo">
+            <a href="http://www.creative-tim.com" class="simple-text">
+                <img src='assets/images/logo_login.png' style='width:120px;' id='gambar'>
+            </a>
+        </div>
+
+        <ul class="nav">
+            <li>
+                <a href="dashboard.php">
+                    <i class="pe-7s-graph"></i>
+                    <p>Dashboard</p>
+                </a>
+            </li>
+            <li>
+                <a href="user.php">
+                    <i class="pe-7s-user"></i>
+                    <p>User Profile</p>
+                </a>
+            </li>
+            <li>
+                <a href="product.php">
+                    <i class="pe-7s-note2"></i>
+                    <p>Manage Product</p>
+                </a>
+            </li>
+            <li>
+                <a href="order.php">
+                    <i class="pe-7s-cash"></i>
+                    <p>List Order</p>
+                </a>
+            </li>
+            <li>
+                <a href="backup.php">
+                    <i class="pe-7s-cloud-download"></i>
+                    <p>Backup</p>
+                </a>
+            </li>
+            <!-- <li>
+                <a href="icons.html">
+                    <i class="pe-7s-science"></i>
+                    <p>Icons</p>
+                </a>
+            </li> -->
+        </ul>
+    </div>
+</div>
