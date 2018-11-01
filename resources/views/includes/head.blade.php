13c3cc9e71e14b2a17a66c91e95bb80376444438
Yose purnawan <purnawan.yose@gmail.com>
2018-11-02 00:15:44 +0700

    Initial Project

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,30 @@
+<meta charset="utf-8" />
+<link rel="icon" type="image/png" href="assets/images/logo_login.png">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+<title>Eximus - TokoPintar</title>
+
+<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+<meta name="viewport" content="width=device-width" />
+
+ <!-- Fonts -->
+ <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+<!-- Bootstrap core CSS     -->
+<link href="{{ url('/css/bootstrap.min.css') }}" rel="stylesheet" />
+
+<!-- Animation library for notifications   -->
+<link href="{{ url('/css/animate.min.css') }}" rel="stylesheet"/>
+
+<!--  Light Bootstrap Table core CSS    -->
+<link href="{{ url('/css/light-bootstrap-dashboard.css?v=1.4.0') }}" rel="stylesheet"/>
+
+
+<!--  CSS for Demo Purpose, don't include it in your project     -->
+<link href="{{ url('/css/demo.css') }}" rel="stylesheet" />
+
+
+<!--     Fonts and icons     -->
+<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+<link href="{{ url('/css/pe-icon-7-stroke.css') }}" rel="stylesheet" />
